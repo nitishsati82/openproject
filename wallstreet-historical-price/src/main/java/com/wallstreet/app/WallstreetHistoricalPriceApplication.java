7c7970f7ef61e7390060a0d6e43ca77011ca7216
Nitish <nitishsati8@gmail.com>
2020-07-07 13:07:58 +0530

    wallstreet jounral historical data csv downloader based on name and start,end date

---
+++ wallstreet-historical-price/src/main/java/com/wallstreet/app/WallstreetHistoricalPriceApplication.java
@@ -0,0 +1,13 @@
+package com.wallstreet.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WallstreetHistoricalPriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WallstreetHistoricalPriceApplication.class, args);
+	}
+
+}
---
+++ wallstreet-historical-price/src/test/java/com/wallstreet/app/WallstreetHistoricalPriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallstreet.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WallstreetHistoricalPriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
